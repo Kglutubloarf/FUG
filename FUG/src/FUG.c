bd255a6dceb6acbd9248097af63f0b7edb421679
Kglutubloarf <toussainthoche@gmail.com>
2016-07-11 15:59:57 +0200

    Restart.

---
+++ FUG/src/FUG.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : FUG.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	system("gnuplot Script.txt");
+	return EXIT_SUCCESS;
+}
